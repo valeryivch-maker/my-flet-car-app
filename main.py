1cf4979af0560ddae910313aca627366b65065aa
Валерий <valeryivch@gmail.com>
2026-07-21 20:32:38 +0300

    ix(main): isolate native android cloud download action from windows none type logic

--- main.py
+++ main.py
@@ -266,7 +266,7 @@ def main(page: ft.Page):
                     scroll=ft.ScrollMode.AUTO, spacing=5, vertical_alignment=ft.CrossAxisAlignment.CENTER,
                     controls=[
                         ft.IconButton(ft.Icons.CLOUD_UPLOAD, tooltip="Экспорт базы в Telegram", on_click=lambda _: network.auto_export_file_to_telegram(page, show_message) if os.name == 'nt' or 'network' in sys.modules else None),
-                        ft.IconButton(ft.Icons.CLOUD_DOWNLOAD, tooltip="Импорт базы данных", on_click=lambda _: [run_local_telegram_sync(), page.data.update({"db_data": engine.load_data()}), rebuild_ui(), show_message("✅ База импортирована!")] if os.name == "nt" else [network.auto_import_last_file(page, show_message) if os.name == 'nt' or 'network' in sys.modules else None]),
+        ft.IconButton(ft.Icons.CLOUD_DOWNLOAD, tooltip="Импорт базы данных", on_click=lambda _: network.auto_import_last_file(page, show_message)),
                         ft.IconButton(ft.Icons.BAR_CHART_ROUNDED, tooltip="Аналитика", on_click=lambda _: [engine.app_state.update({'view_mode': 'analytics' if engine.app_state.get('view_mode') != 'analytics' else 'list'}), rebuild_ui()]),
                         ft.VerticalDivider(width=10, color=ft.Colors.BLACK_12),
                         ft.IconButton(ft.Icons.ADD_CIRCLE, tooltip="Добавить авто", on_click=add_car_click),
